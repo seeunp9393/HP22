539e433b40f1522b17f83099e6998f752a3af138
seeunp9393 <72725533+seeunp9393@users.noreply.github.com>
2020-11-05 15:18:59 +0900

    20201105
    2020110520201105

---
+++ js/jq.js
@@ -0,0 +1,26 @@
+$(".burger-icon").click(function (event) {
+  $(".menu_wrap").fadeIn(700);
+});
+const burgerMenuC = $(".wrap").click(function (event) {
+  $(".menu_wrap").fadeOut(500);
+});
+
+const search = $(".search-icon").click(function (event) {
+  alert("미구현");
+});
+
+const cart = $(".cart-icon").click(function (event) {
+  alert("장바구니에 물건이 없습니다.");
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $("#backToTop").fadeIn(200);
+  } else {
+    $("#backToTop").fadeOut(200);
+  }
+});
+$("#backToTop").click(function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, 200);
+});
